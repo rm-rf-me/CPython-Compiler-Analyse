b3c053e9ae8d982d3574797767869cc020dee4c3
liou <1127429568@qq.com>
2021-07-14 17:58:19 +0800

    add cpython3.8 code

---
+++ cpython-3.8实验代码/test.py
@@ -0,0 +1,33 @@
+# test.py
+import symbol
+import token
+import parser
+from pprint import pprint
+import dis
+import ast
+
+def lex(expression):
+    symbols = {v: k for k, v in symbol.__dict__.items() if isinstance(v, int)}
+    tokens = {v: k for k, v in token.__dict__.items() if isinstance(v, int)}
+    lexicon = {**symbols, **tokens}
+    st = parser.expr(expression)
+    st_list = parser.st2list(st)
+
+    def replace(l: list):
+        r = []
+        for i in l:
+            if isinstance(i, list):
+                r.append(replace(i))
+            else:
+                if i in lexicon:
+                    r.append(lexicon[i])
+                else:
+                    r.append(i)
+        return r
+
+    return replace(st_list)
+
+test_s = 'a++'
+pprint(lex(test_s))
+print(ast.dump(ast.parse(test_s)))
+tmp = dis.dis(test_s)
